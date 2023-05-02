1a5df47200a9f978e6d35a9890d368ca49ea8ee5
mOHAMMEDaLREZQ <alrezq@vt.edu>
2023-05-01 22:33:12 -0400

    Int. exploratory (Cont'd)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # VehicleInspection
+
+Data was obtained from https://catalog.data.gov/dataset/public-passenger-vehicle-inspection-schedule
--- VehicleInspectionData.R
+++ VehicleInspectionData.R
@@ -1,5 +1,11 @@
 
+
+# Loading the dataset  ----------------------------------------------------
+
 car_inpsection <- read.csv("Public_Passenger_Vehicle_Inspection_Schedule.csv")
+
+
+# Initial exploratory analysis  -------------------------------------------
 
 str(car_inpsection)
 
